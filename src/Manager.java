c6bde02893db042b304ed22dfbbdb46deddd2b7b
Abdul Gofur Tiflen <freakfrozen01@gmail.com>
2025-01-20 09:04:45 +0700

    method overriding

--- src/Manager.java
+++ src/Manager.java
@@ -2,7 +2,7 @@ public class Manager {
   String nama;
 
   void sayHello(String nama) {
-    System.out.println("Hello " + nama + "My Name is " + this.nama);
+    System.out.println("Hello " + nama + " My Name is Manager " + this.nama);
   }
 
 
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,2 +1,7 @@
 public class VicePresident extends Manager{
+
+  void sayHello(String nama) {
+    System.out.println("Hello " + nama + " My Name is VP " + this.nama);
+  }
+  
 }
